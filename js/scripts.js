8675e3ac59a54592059bbefe5649efbe6ea548cc
Vidit Khandelwal <viditk17@gmail.com>
2020-04-14 05:13:27 +0000

    Changes to Mobile header design and secondary navigation design

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-/*unction alertMessage(){
+function alertMessage(){
    alert("This website is under construction. Please do not proceed");
-}*/
-
+}
 function menu(){
   if(window.innerWidth < 700)
   {
     document.getElementById("menu-icon").style.display='inline';
     document.getElementById("primary-nav-links").style.display='none';
-		var prim_nav_elements = document.getElementsByClassName("primary-nav");
+		document.getElementById("primary-nav").style.padding='10px 25px 10px 25px';
+		/*var prim_nav_elements = document.getElementsByClassName("primary-nav");
 		var i;
 		for(i=0;i<prim_nav_elements.length;i++)
 		{
@@ -15,7 +15,7 @@ function menu(){
 			prim_nav_elements[i].paddingRight='25px';
 			prim_nav_elements[i].paddingBottom='10px';
 			prim_nav_elements[i].paddingLeft='25px';
-		}
+		}*/
   }
 }
 function HamburgerMenu(){
@@ -29,4 +29,20 @@ function HamburgerMenu(){
     document.getElementById("primary-nav-links-mobile").style.display='none';
 		document.getElementById("menu-icon").innerHTML='<img src="/assets/HamburgerMenu-VKCS-2.svg" alt="Hamburger Menu Icon">'
   }
+}
+function secnavpages(){
+	if(window.loaction.indexOf("#")<0)
+	window.loaction+="#";
+}
+function secnavdrop(){
+	if(document.getElementById("secnav").style.display=='none')
+	{
+		document.getElementById("secnav").style.display='block';
+		document.getElementById("secnavdrop").innerHTML='Close Menu<br>';
+	}
+	else
+	{
+		document.getElementById("secnav").style.display='none';
+		document.getElementById("secnavdrop").innerHTML='OOP Menu<br>';
+	}
 }
